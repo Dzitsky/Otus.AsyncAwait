4b3daa406498ea27e8bff0a75279a64dd80bee67
Automerge <automerge@rts-tender.ru>
2022-11-02 01:27:56 +0300

    Add Acync Samples

--- Otus.AsyncAwait/Program.cs
+++ Otus.AsyncAwait/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Otus.AsyncAwait
+﻿using System.Diagnostics;
+using System.Net.Security;
+using System.Text;
+
+namespace Otus.AsyncAwait
 {
+
+
     internal class Program
     {
-        static void Main(string[] args)
+        private static string Message = "Message";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SaySomething();
+            Console.WriteLine(Message);
+
+            //IProgram program = new GettingTaskWithoutAwaitProgram();
+            //await program.ExecuteAsync();
+
+            //program = new ParallelTasksProgram();
+            //await program.ExecuteAsync();
+
+            //program = new ExceptionDemoProgram();
+            //await program.ExecuteAsync();
+
+            //program = new CancellationTaskProgram();
+            //await program.ExecuteAsync();
+
+            return;
+        }
+
+        static async Task<string> SaySomething()
+        {
+            await Task.Delay(5);
+            Message = "Hello!";
+            return "bla-bla-bla";
         }
     }
 }
